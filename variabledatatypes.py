cec7cd91825f3f391ba36c083fbc309f47deacdc
Luke Francis <lukebrandonfrancisdev@gmail.com>
2020-04-13 14:16:58 -0400

    variable datatypes

---
+++ variabledatatypes.py
@@ -0,0 +1,10 @@
+character_name = "John"
+character_age = 35
+is_male = True
+
+print(f"There once was a man named {character_name}, ")
+print(f"he was {character_age} years old ")
+
+character_name = "Mike"
+print(f"He really like the name {character_name}, ")
+print(f"but didn't like being {character_age}.")
